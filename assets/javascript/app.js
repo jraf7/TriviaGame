4dcd72f6d82eb4d6cb4e20a8f55ef6bdc388f9c3
Jonathan Johnson <jraf7@icloud.com>
2019-10-30 22:57:28 -0400

    restart project. reinitialize cow theme.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,66 @@
 
 $(document).ready(function(){
 
+$(".content").hide();
+
+$("#startButton").on("click", startTimer);
+let correct = 0;
+let incorrect = 0; 
+let unanswered = 0;
+
+let timeLeft = 120;
+function startTimer (){
+     $("#time").text(timeLeft + " seconds");
+     setInterval(countdown, 1000);
+     $("#startButton").hide();
+     $(".content").show();
+     $(".results").hide();
+     displayQuestions();
+ };
+
+function countdown (){
+ timeLeft--;
+ $("#time").text(timeLeft);
+ if(timeLeft == 0) {
+     stopTimer();
+     $("#time").empty();
+     endDisplay();
+ }
+ };
+function stopTimer (){
+    clearInterval();
+    checkAnswers();
+    $("#time").empty();
+ };
+
+ function endDisplay (){
+     $(".results").show();
+
+
+ };
+
+ function displayQuestions (){
+    for (let i = 0; i < questionsArr; i++){
+        let questions = questionsArr[i].questions
+        $(".qSpace").text(questions);
+        let answers = questionsArr[i].answers
+        for (let j = 0; j < answers.length; j++){
+            answers[j]
+        }
+    }
+ };
+
+ function checkAnswers (){
+
+ };
+
+
+
+
+
+
+
+
 const questionsArr = [
     [   {"questions": "What is cow meat called?"},
         {"correct": "beef" }],
@@ -15,29 +75,4 @@ const questionsArr = [
         { "correct" : "true" }],
         {"answers" : ["true", "false"]}
     ];
-
-let correct = 0;
-let incorrect = 0; 
-let unanswered = 0;
-let timer = 0;
-
-
-$(".timer-div").hide();
-$(".answer").hide();
-$("#startbtn").show();
-$("#subbtn").hide();
-
-// Start Btn Press
-$("#startbtn").on("click", function(){
-
-$(".timer-div").show();
-$(".answers").show();
-$("#startbtn").hide();
-$("#subbtn").show();
-$
-
-
-});
-
-});
-
+})
