194432591b6a9d6a1c431676a54386e0ccf4c4dd
Jonathan Johnson <jraf7@icloud.com>
2019-12-20 00:00:14 -0500

    complete logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,77 +1,111 @@
+let countdown = 25;
+let right = 0;
+let wrong = 0;
 
-$(document).ready(function(){
-
-$(".content").hide();
-
-$("#startButton").on("click", startTimer);
-let correct = 0;
-let incorrect = 0; 
-let unanswered = 0;
-let timeLeft = 10;
-function startTimer (){
-     $("#time").text(timeLeft + " seconds");
-     setInterval(countdown, 1000);
-     $("#startButton").hide();
-     $(".content").show();
-     $(".results").hide();
-     displayQuestions();
- };
-
-function countdown (){
- timeLeft--;
- $("#time").text(timeLeft);
- if(timeLeft == 0) {
-     stopTimer();
-     $("#time").empty();
-     endDisplay();
-    }  
- };
-function stopTimer (){
-    clearInterval();
-    checkAnswers();
-    $("#time").empty();
- };
-
- function endDisplay (){
-     $(".results").show();
+renderStartButton();
 
 
- };
+function renderStartButton () {
+    let startButton = $("<button>")
+        .text("Start!")
+        .addClass("buttonClass btn btn-lrg btn-secondary")
+        .on("click", function (){
+            renderQuestions();
+        })
 
- function displayQuestions (){
-    for (let i = 0; i < questionsArr; i++){
-        let questions = questionsArr[i].questions
-        $(".qSpace").text(questions);
-        let answers = questionsArr[i].answers
-        for (let j = 0; j < answers.length; j++){
-            answers[j]
+    $("#content").append(startButton);
+};
+
+function renderQuestions () {
+    $("#content").empty();
+    let timer = $("<div>").text(`Time Remaining: ${countdown}`);
+    $("#content").append(timer);
+    interval = setInterval(function () {
+        if(countdown === 0) {
+            clearInterval(interval);
+            getResult();
+            renderResult();
+        } else {
+            countdown--;
+            timer.text(`Time Remaining: ${countdown}`);
         }
+    }, 1000);
+
+    questionsArr.forEach( function (question) {
+        let mcQuestion = $("<div>").addClass("question");
+        let prompt = $("<div>").text(question[0].questions);
+        mcQuestion.append(prompt);
+        let set = $("<fieldset>").attr("id", `group${question[3].id}`);
+        for (let i = 0; i < 4; i++){
+            let label = $("<label class='radio-inline'>")
+                .html(`<input key='${i}' type="radio" name="group${question[3].id}" value='${question[2].answers[i]}'> ${question[2].answers[i]}`)
+                .css('font-size', '14px').css('margin-right', '20px');
+            set.append(label);
+        };
+        mcQuestion.append(set);
+        $("#content").append(mcQuestion);
+    })
+    let finishButton = $("<button>").text("Finish!").addClass("buttonClass btn btn-lrg btn-secondary");
+    finishButton.on("click", function () {
+        clearInterval(interval);
+        getResult();
+        renderResult();
+    });
+    $("#content").append(finishButton);
+};
+
+function getResult () {
+   let checked = $(":checked");
+   for (let i = 0; i < checked.length; i++) {
+           if (checked[i].value === questionsArr[i][1].correct) {
+        right++;
+       } else {
+        wrong++;
+       }
+   };
+};
+
+function renderResult () {
+    $("#content").empty();
+    let outro = $("<h3>");
+    if (right > wrong) {
+        outro.text("Winner. You're a smart cookie!")
+    } else {
+        outro.text("Loser. You're a mad cow.")
     }
- };
-
- function checkAnswers (){
-    
- };
-
-
-
-
-
-
+    let wins = $("<h4>").text(`Correct: ${right}`);
+    let losses = $("<h4>").text(`Wrong: ${wrong}`);
+    let retry = $("<button>").on("click", function () {
+        wrong = 0;
+        right = 0;
+        countdown = 25;
+        $("#content").empty();
+        renderStartButton();
+    });
+    retry.text("Try Again?")
+    $("#content").append(outro, wins, losses, retry);
+}
 
 
 const questionsArr = [
     [   {"questions": "What is cow meat called?"},
-        {"correct": "beef" }],
-        {"answers" : ["beef", "poultry", "pork", "fish"]}
-    [   {"questions": "Cows naturally have 4 legs. True or False" },
-        {"correct": "true" }],       
-        {"answers" : ["true", "false"]}
-    [   { "questions": "Cows have 32 teeth. True or False"},
-        { "correct": "true" }],
-        {"answers" : ["true", "false"]}
-    [   { "questions": "Female cows produce milk."},
-        { "correct" : "true" }],
-        {"answers" : ["true", "false"]}
-    ];
-})
+        {"correct": "beef" },
+        {"answers" : ["beef", "poultry", "pork", "fish"]},
+        {"id": 1}
+    ],
+    [   {"questions": "Cows naturally have how many legs?" },
+        {"correct": "4" },
+        {"answers" : ["1", "2", "3", "4"]},
+        {"id": 2}
+    ],
+    [   { "questions": "What is a cow with no legs called?"},
+        { "correct": "ground beef" },
+        {"answers" : ["ground beef", "shoe", "tuna", "birds"]},
+        {"id": 3}
+    ],
+    [   { "questions": "Cows are what type of animal?"},
+        { "correct" : "mammals" },
+        {"answers" : ["mythical", "mammals", "reptiles", "angiosperms"]},
+        {"id": 4}
+    ]
+];
